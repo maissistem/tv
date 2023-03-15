3aa8aa3fbbe8003447c345d66bdce80c5b6cb9e0
Mais Sistem Soluções Empresariais <maissistem@gmail.com>
2023-03-15 00:45:23 -0300

    servidor funfando...

--- src/server.js
+++ src/server.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const connection = require('./db');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/api/products', async (req, res) => {
-  const [rows] = await connection.query('SELECT * FROM produtos');
-  res.json(rows);
+const connection = mysql.createPool({
+  host: '185.169.96.240',
+  user: 'maissist_jk',
+  password: '314159@Mais#',
+  database: 'maissist_jkastro',
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.json());
+app.use(cors());
+
+app.get('/produtos', async (req, res) => {
+  try {
+    const [rows] = await connection.query('SELECT * FROM produtos');
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Erro interno no servidor' });
+  }
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+app.get('/produtos/:id', async (req, res) => {
+  try {
+    const [rows] = await connection.query('SELECT * FROM produtos WHERE id = ?', [req.params.id]);
+    if (rows.length > 0) {
+      res.json(rows[0]);
+    } else {
+      res.status(404).json({ message: 'Produto não Localizado' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Erro interno no servidor' });
+  }
 });
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Servidor Rodando na Porta ${PORT}`));
